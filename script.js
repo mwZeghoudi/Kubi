b51a6066df65c264d775c73f60b4a2fbb9aa2fd3
Mohammed-Walid Zeghoudi <mwzeghoudi@santeformapro.com>
2024-07-30 17:54:35 +0200

    [ADD] SWIPER

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ accordionBtns.forEach(el => {
     });
   })
 })
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
